497d440d22f074181daa0a6815b39bff3db15017
garaoh70 <122757758+garaoh70@users.noreply.github.com>
2025-05-05 23:07:05 +0900

    Day 7 : mac に開発を移行したところ文字化けする問題を解決できない。一旦終了

---
+++ week01/day07-summary.hs
@@ -0,0 +1,29 @@
+﻿{-# LANGUAGE OverloadedStrings #-}
+
+import System.IO
+import GHC.IO.Encoding
+
+-- Day 7：1週間まとめ
+main = do
+    setLocaleEncoding utf8
+    print "世界"
+ 
+    print $ sample01 (-5)
+    print $ sample01 3
+    print $ sample01 0
+
+    print $ sample02 "Hello"
+    print $ sample02 "世界"
+    print $ sample02 "ABC"
+
+-- 整数を入力して絶対値を返す関数を作成する。
+-- ただし、関数内で ガード を使うこと。
+sample01 x
+    | x > 0 = x
+    | x < 0 = (-x)
+    | otherwise = 0
+
+-- 文字列を逆さまにして、先頭に「うぎゃー！」をつける関数を作成する。
+-- ただし、reverse と ++ を使うこと。
+sample02 :: String -> String
+sample02 x = "うぎゃー！" ++ (reverse x)
