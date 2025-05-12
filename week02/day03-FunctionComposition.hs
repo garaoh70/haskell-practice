1818da977ba50b517b3fec56894f37cbe7f62b8e
garaoh70 <122757758+garaoh70@users.noreply.github.com>
2025-05-12 23:29:05 +0900

    Day3: 関数合成とポイントフリー

---
+++ week02/day03-FunctionComposition.hs
@@ -0,0 +1,13 @@
+﻿-- Day 3 : 関数合成とポイントフリースタイル
+main = do
+    let dat = [5, -3, -6, 7, -3, 2, -19, 24]
+    -- 配列の要素を全て負の値に変換する
+    print $ map(\x -> negate (abs x)) dat
+    -- 関数合成
+    print $ map(negate . abs) dat
+
+    -- ポイントフリースタイル(奇数の平方根のうち10000より小さい数の総和を求める)
+    print $ sum . takeWhile(<10000) $ map (^2) (filter odd [1..])
+    -- 結果が同じなら、順番を入れ替えて読みやすくするのだそうです！！
+    -- Haskell の世界は奥が深い
+    print $ sum . takeWhile(<10000) . filter odd $ map (^2) [1..]
